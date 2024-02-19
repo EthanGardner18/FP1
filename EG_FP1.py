6fa579fa80a524211615cc3511d93e3d00a6709f
Ethan Gardner <etgardn@siue.edu>
2024-02-19 09:54:12 -0600

    done

--- EG_FP1.py
+++ EG_FP1.py
@@ -1,28 +1,35 @@
-def assignHole(mices, holes, n, m):
-    if (n != m):
+def MTH(mices, holes, n, m):
+    # If there are more mice then holes stop
+    if (m > n):
         return -1
     
+    # Start by sorting the mice and the holes. Then see the max difference between each one for the longest time
     mices.sort()
     holes.sort()
     
-    Max = 0
-     
+    # Set the largest distance to 0 
+    max = 0
+    
+    #compares the location of the mice to the holes
     for i in range(n):
-        if (Max < abs(mices[i] - holes[i])):
-            Max = abs(mices[i] - holes[i])
+        if (max < abs(mices[i] - holes[i])):
+            max = abs(mices[i] - holes[i])
      
-    return Max
+    return max
 
 def main():
-    mices = [ 4, -4, 2 ]
+    # Position of mice
+    mice = [ 4, 9, 2 ]
     holes = [ 4, 0, 5 ]
 
-    n = len(mices)
+    # Num of holes and num of mice
+    n = len(mice)
     m = len(holes) 
 
-    minTime = assignHole(mices, holes, n, m)
+    # Time for all mice to find a hole
+    time = MTH(mice, holes, n, m)
 
-    print("The last mouse gets into the hole in time:", minTime)
+    print("Total Time was ", time)
 
 if __name__ == "__main__":
     main()
